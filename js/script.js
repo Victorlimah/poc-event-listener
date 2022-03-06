ebaff5454c4655deeaaa911bf8056c39eb787eb5
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-03-05 22:53:12 -0300

    finish example 'click'

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // JS
+
+// FUNCTION CLICK
+const elementClick = document.querySelector(".elemento-click");
+elementClick.addEventListener("click", alertClick);
+
+function alertClick() {
+  alert("Você clicou na parada");
+}
