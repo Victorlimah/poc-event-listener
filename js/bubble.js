934d9196ef9c60269a4009ed4be68a356cc58049
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-03-07 19:33:02 -0300

    finish

--- js/bubble.js
+++ js/bubble.js
@@ -4,9 +4,9 @@ const neto = document.getElementById("neto");
 
 pai.addEventListener("click", (x) => console.log("pai foi clicado"));
 filho.addEventListener("click", (x) => console.log("filho foi clicado"));
-neto.addEventListener("click", (x) => console.log("neto foi clicado"));
+//neto.addEventListener("click", (x) => console.log("neto foi clicado"));
 
-// neto.addEventListener("click", (x) => {
-//   console.log("neto foi clicado");
-//   event.stopPropagation();
-// });
+neto.addEventListener("click", (x) => {
+  console.log("neto foi clicado");
+  event.stopPropagation();
+});
